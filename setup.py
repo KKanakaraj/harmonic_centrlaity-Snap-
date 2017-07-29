185cd33f052fec22b8704a6b7eb661caee9b182b
skkamal95 <skkamal95@gmail.com>
2017-07-29 17:46:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='harmonic_centrality_snap',
       version='0.2',
        description='Harmonic Centrality metric realization for snap',
        author='Kamalesh Kumar',
-      url='https://github.com/asash/harmonic_centrality',
+      url='https://github.com/skkamal95/harmonic_centrlaity-Snap-',
        packages=['harmonic_centrality_snap'],
        install_requires = [
               'Snap',
